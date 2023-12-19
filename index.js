4c3038fb572cf9889978071f8d0fc9fc2e235d3c
Allen Annom <49309320+allenannom@users.noreply.github.com>
2023-12-19 19:30:22 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 // Import packages
 const express = require("express");
+const cors = require('cors');
 const home = require("./routes/home");
 const openaiChat = require("./routes/openaiChat");
 // Middlewares
 const app = express();
 app.use(express.json());
+
+var corsOptions = {
+  origin: 'https://chat.legal-quant.com', // or use '*' to allow all origins
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 
 // Routes
 app.use("/home", home);
