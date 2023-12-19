3ba1bde9271b14f7604777b3dc1f0da6a522f2c3
Allen Annom <49309320+allenannom@users.noreply.github.com>
2023-12-19 18:57:47 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Import packages
 const express = require("express");
 const home = require("./routes/home");
-
+const openaiChat = require("./routes/openaiChat");
 // Middlewares
 const app = express();
 app.use(express.json());
 
 // Routes
 app.use("/home", home);
-
+app.use("/openaiChat", openaiChat);
 // connection
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
